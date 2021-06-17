3507e94162d331986ff5c7ce8753502ebd1e586b
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-06-17 21:43:00 +0100

    adding generalError

--- generalError.js
+++ generalError.js
@@ -1,8 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+
 class GeneralError extends Error {
-    constructor(message, status){
+    constructor(message, httpCode, description) {
         super(message);
-        this.status = status;
+        this.httpCode = httpCode;
+        this.description = description;
+        Error.captureStackTrace(this, this.constructor);
     }
 
+    static logError(err) {
+        const fileName = `${new Date().toUTCString()} `;
+        const logDir = path.join(__dirname, `./errorLogs/${fileName}`);
+        const errLog = `
+        ----------- ERROR LOG  START -----------\n
+        ERROR_MESSAGE: "${err.message}"\n
+        HTTP_CODE: "${err.httpCode}"\n
+        DESCRIPTION: "${err.description}"\n
+        STACK: ${err.stack}\n
+        ----------- ERROR LOG  END -----------`;
 
+        fs.writeFile(logDir, errLog, 'utf8', err => console.log(err));
+    }
 }
+
+class APIError extends GeneralError { }
+class MongooseError extends GeneralError { }
+
+module.exports = GeneralError;
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const GeneralError = require('./generalError');
 
 const app = express();
 
-app.get('/apiError', (req, res) => {
-    try 
-})
+GeneralError.logError({message: 'message', httpCode: 500, description: 'description'});
+
+
+app.listen(3000, (err) => {
+    if (err) console.log('Server failed to listen');
+    console.log('Server listening on port 3000');
+});
